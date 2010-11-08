e40912d1dd0c6fa34da8939c1c28c9e0d9f683b0
Nerijus <gaivenis.nerijus@gmail.net>
2010-11-09 00:59:40 +0200

    test

--- moduliai/apie_mus.php
+++ moduliai/apie_mus.php
@@ -17,7 +17,7 @@
 <ul style="list-style-image: url(paslaugos/ok.gif)" >
 <li style="line-height:30px;" >Kainą nustatysite Jūs.</li>
 <li style="line-height:30px">Atlikdami darbą daug dėmesio skiriame smulkmenoms.</li>
-<li style="line-height:30px">Mes laikome darbą užbaigtą tik tada kai klientas lieka patenkintas.</li>
+<li style="line-height:30px">blt neweikia Mes laikome darbą užbaigtą tik tada kai klientas lieka patenkintas.</li>
 <li style="line-height:30px">Mes laikome darbą užbaigtą tik tada kai klientas lieka patenkintas.</li><li style="line-height:30px">Mes laikome darbą užbaigtą tik tada kai klientas lieka patenkintas.</li>
 </ul>
 
